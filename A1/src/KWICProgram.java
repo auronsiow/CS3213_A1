8b49c359d919d5282d2a70f9993de064993d554a
auron <auron@192.168.0.11>
2014-08-28 09:59:57 +0800

    Refactor input processing in main program

--- A1/src/KWICProgram.java
+++ A1/src/KWICProgram.java
@@ -25,31 +25,14 @@ public class KWICProgram {
 	
 		displayWelcomeText();
 		
-		while (true) {
+		boolean isPlaying = true;
+		
+		while (isPlaying) {
+			
 			LinkedList<String> ll_LinesToCheck = new LinkedList<String>();
 			LinkedList<String> ll_IllegalKeywordList = new LinkedList<String>();
 			
-			// Get # of lines of text
-			System.out.print(PROMPT_NUM_LINES);
-			int numLines = sc.nextInt();
-			
-			// Clear the buffer
-			sc.nextLine();
-			
-			// Get each individual input of text
-			for(int i=0; i < numLines; i++) {
-				System.out.print("Enter line " + (i+1) + " of " + numLines + ": ");
-				String input = sc.nextLine();
-				ll_LinesToCheck.add(input);
-			}
-			
-			// Get the string of illegal words
-			System.out.print(PROMPT_ILLEGAL_WORDS);
-			String str_inputIllegal = sc.nextLine();
-			
-			StringTokenizer st = new StringTokenizer(str_inputIllegal);
-			while(st.hasMoreTokens())
-				ll_IllegalKeywordList.add(st.nextToken().toLowerCase());
+			getUserInput(sc, ll_LinesToCheck, ll_IllegalKeywordList);
 			
 			// Generate Circular lines
 			CircularGenerator f_Circular = new CircularGenerator();
@@ -67,9 +50,7 @@ public class KWICProgram {
 			printKWICOutput(ll_LinesToCheck);
 
 			// Get user's feedback to exit or replay
-			System.out.println(PROMPT_REPLAY_KEY + REPLAY_KEY + PROMPT_TERMINATE_KEY);
-			if(!sc.nextLine().contains(REPLAY_KEY))
-				break;
+			isPlaying = getUserReplay(sc);
 		}
 		
 		System.out.println(PROMPT_TERMINATION);
@@ -88,6 +69,45 @@ public class KWICProgram {
 		catch (Exception e1){
 			e1.printStackTrace();
 		}
+	}
+	
+	public static boolean getUserReplay(Scanner sc ) {
+		System.out.println(PROMPT_REPLAY_KEY + REPLAY_KEY + PROMPT_TERMINATE_KEY);
+		if(sc.nextLine().contains(REPLAY_KEY))
+			return true;
+		else
+			return false;
+	}
+	
+	public static void getUserInput(Scanner sc, LinkedList<String> ll_LinesToCheck, LinkedList<String> ll_IllegalKeywordList) {
+		getNumberOfLines(sc, ll_LinesToCheck);
+		getIllegalWords(sc, ll_IllegalKeywordList);
+	}
+	
+	public static void getNumberOfLines(Scanner sc, LinkedList<String> ll_LinesToCheck) {
+		// Get # of lines of text
+		System.out.print(PROMPT_NUM_LINES);
+		int numLines = sc.nextInt();
+
+		// Clear the buffer
+		sc.nextLine();
+
+		// Get each individual input of text
+		for(int i=0; i < numLines; i++) {
+			System.out.print("Enter line " + (i+1) + " of " + numLines + ": ");
+			String input = sc.nextLine();
+			ll_LinesToCheck.add(input);
+		}
+	}
+	
+	public static void getIllegalWords(Scanner sc, LinkedList<String> ll_IllegalKeywordList) {
+		// Get the string of illegal words
+		System.out.print(PROMPT_ILLEGAL_WORDS);
+		String str_inputIllegal = sc.nextLine();
+
+		StringTokenizer st = new StringTokenizer(str_inputIllegal);
+		while(st.hasMoreTokens())
+			ll_IllegalKeywordList.add(st.nextToken().toLowerCase());
 	}
 	
 	public static void printKWICOutput(LinkedList<String> ll) {
