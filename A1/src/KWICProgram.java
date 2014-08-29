0da66cf0136f989590b8f0edc3080ac642dc2db3
auron <auron@192.168.0.11>
2014-08-29 17:47:07 +0800

    Integration of all components into pipeline

--- A1/src/KWICProgram.java
+++ A1/src/KWICProgram.java
@@ -11,40 +11,36 @@ import java.io.*;
 
 public class KWICProgram {	
 
-	public static final String REPLAY_KEY = "y";
 	public static final String WELCOME_TEXT_FILE_PATH = "other_resources/help.txt";
-	public static final String PROMPT_TERMINATION = "---Thank you for using KWIC---";
-	public static final String PROMPT_NUM_LINES = "Enter number of lines: ";
-	public static final String PROMPT_ILLEGAL_WORDS = "Enter illegal words: ";
+	public static final String REPLAY_KEY = "y";
 	public static final String PROMPT_REPLAY_KEY = "To replay, press \"";
 	public static final String PROMPT_TERMINATE_KEY = "\". Otherwise, press any key to exit.";
-	public static final String PROMPT_OUTPUT = "Output: ";
+	public static final String PROMPT_TERMINATION = "---Thank you for using KWIC---";
+	public static final String TEST_CASE_PATH = "other_resources/TestCase3_LargeUnRes.txt"; 
 
 	public static void main(String arg[]) {
-		Scanner sc = new Scanner(System.in);
-	
 		displayWelcomeText();
 		
-		boolean isPlaying = true;
-		
-		while (isPlaying) {
+		while (true) {
 			
-			LinkedList<String> ll_LinesToCheck = new LinkedList<String>();
-			LinkedList<String> ll_IllegalKeywordList = new LinkedList<String>();
+			KWICPipeline pipeline = new KWICPipeline();
+			DataSource srcComponent = new DataSource();
+			CircularGenerator cirComponent = new CircularGenerator();
+			IllegalWordFilter illComponent = new IllegalWordFilter();
+			AlphabetSort sortComponent = new AlphabetSort();
+			OutputSink sinkComponent = new OutputSink();
 			
-			getUserInput(sc, ll_LinesToCheck, ll_IllegalKeywordList);
+			pipeline.addLink(srcComponent);
+			pipeline.addLink(cirComponent);
+			pipeline.addLink(illComponent);
+			pipeline.addLink(sortComponent);
+			pipeline.addLink(sinkComponent);
 			
-			// Generate Circular lines
+			pipeline.process();
 			
-			// Sort Circular lines alphabetically
-			
-			// Remove illegal keywords found
-			
-			// Display output
-			printKWICOutput(ll_LinesToCheck);
-
 			// Get user's feedback to exit or replay
-			isPlaying = getUserReplay(sc);
+			if (!playAgain())
+				break;
 		}
 		
 		System.out.println(PROMPT_TERMINATION);
@@ -64,54 +60,29 @@ public class KWICProgram {
 			e1.printStackTrace();
 		}
 	}
-	
-	public static boolean getUserReplay(Scanner sc ) {
+
+	public static boolean playAgain() {
+
+		Scanner sc = new Scanner(System.in);
 		System.out.println(PROMPT_REPLAY_KEY + REPLAY_KEY + PROMPT_TERMINATE_KEY);
-		if(sc.nextLine().contains(REPLAY_KEY))
-			return true;
-		else
+		try {
+			if(sc.nextLine().contains(REPLAY_KEY))
+				return true;
+			else
+				return false;
+		}
+		catch (Exception err) {
 			return false;
-	}
-	
-	public static void getUserInput(Scanner sc, LinkedList<String> ll_LinesToCheck, LinkedList<String> ll_IllegalKeywordList) {
-		getNumberOfLines(sc, ll_LinesToCheck);
-		getIllegalWords(sc, ll_IllegalKeywordList);
-	}
-	
-	public static void getNumberOfLines(Scanner sc, LinkedList<String> ll_LinesToCheck) {
-		// Get # of lines of text
-		System.out.print(PROMPT_NUM_LINES);
-		int numLines = sc.nextInt();
-
-		// Clear the buffer
-		sc.nextLine();
-
-		// Get each individual input of text
-		for(int i=0; i < numLines; i++) {
-			System.out.print("Enter line " + (i+1) + " of " + numLines + ": ");
-			String input = sc.nextLine();
-			ll_LinesToCheck.add(input);
 		}
 	}
 	
-	public static void getIllegalWords(Scanner sc, LinkedList<String> ll_IllegalKeywordList) {
-		// Get the string of illegal words
-		System.out.print(PROMPT_ILLEGAL_WORDS);
-		String str_inputIllegal = sc.nextLine();
-
-		StringTokenizer st = new StringTokenizer(str_inputIllegal);
-		while(st.hasMoreTokens())
-			ll_IllegalKeywordList.add(st.nextToken().toLowerCase());
-	}
-	
-	public static void printKWICOutput(LinkedList<String> ll) {
-		System.out.println(PROMPT_OUTPUT);
-		for(String s : ll) {
-			String firstChar = s.substring(0,1);
-			firstChar = firstChar.toUpperCase();
-			String outStr = firstChar + s.substring(1);
-			System.out.println(outStr);
+	public static void runTestCase() {
+		try {
+			System.setIn(new FileInputStream(TEST_CASE_PATH));
 		}
-		System.out.println();
+		catch (Exception err) {
+			err.printStackTrace();
+		}
 	}
+
 }
